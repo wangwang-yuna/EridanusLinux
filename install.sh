e2483bc05c96cde6f0fef297335e3a5bd83c5e78
wang-yuna <136219691+wangwang-yuna@users.noreply.github.com>
2025-01-11 13:44:55 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ fi
 # 安装 pip 依赖包
 if [ -f requirements.txt ]; then
     echo "安装 pip 依赖包"
-    pip3 install -r requirements.txt
+    pip3 install -r Eridanus/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 else
     echo "requirements.txt 文件不存在!"
     echo "请检查文件路径是否正确!"
